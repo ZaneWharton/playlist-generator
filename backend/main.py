02cabc403b3f061c93539c10d6be6cde181da135
ZaneWharton <zanewharton@gmail.com>
2025-06-29 11:57:30 -0500

    Fixes deploy issues

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from typing import List
 from fastapi import FastAPI, Request, Depends, HTTPException, Body
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse, JSONResponse
 from auth import oauth, get_current_user
 from spotify import search_playlist
@@ -15,6 +16,8 @@ from dotenv import load_dotenv
 app = FastAPI(title="Mood Playlist Generator")
 load_dotenv()
 FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Session middleware for user sessions
 app.add_middleware(
@@ -38,24 +41,6 @@ class SaveRequest(BaseModel):
     name: str
     description: str
     uris: List[str]
-
-@app.get("/auth/login")
-async def login(request: Request):
-    #Redirects the user to the Spotify login page.
-   
-    redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
-    return await oauth.spotify.authorize_redirect(request, redirect_uri=redirect_uri)
-
-@app.get("/auth/callback")
-async def callback(request: Request):
-    #Handles the Spotify OAuth callback and retrieves the access token.
-
-    token = await oauth.spotify.authorize_access_token(request) 
-    user = await oauth.spotify.get('https://api.spotify.com/v1/me', token=token)
-    access_token = token['access_token']
-    request.session['user'] = user.json()
-    request.session['access_token'] = token['access_token']
-    return RedirectResponse(f"{FRONTEND_URL}/#access_token={access_token}")
 
 @app.get("/api/playlist")
 async def get_playlist(mood: str, user=Depends(get_current_user)):
@@ -99,6 +84,24 @@ async def save_playlist( request: Request, user=Depends(get_current_user), paylo
         response.raise_for_status()
 
     return {"url": f"https://open.spotify.com/playlist/{playlist_id}"}
+
+@app.get("/auth/login")
+async def login(request: Request):
+    #Redirects the user to the Spotify login page.
+   
+    redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
+    return await oauth.spotify.authorize_redirect(request, redirect_uri=redirect_uri)
+
+@app.get("/auth/callback")
+async def callback(request: Request):
+    #Handles the Spotify OAuth callback and retrieves the access token.
+
+    token = await oauth.spotify.authorize_access_token(request) 
+    user = await oauth.spotify.get('https://api.spotify.com/v1/me', token=token)
+    access_token = token['access_token']
+    request.session['user'] = user.json()
+    request.session['access_token'] = token['access_token']
+    return RedirectResponse(f"{FRONTEND_URL}/#access_token={access_token}")
 
 @app.post("/auth/logout")
 async def logout(request: Request):
