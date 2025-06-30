74a97583918fefada0b54bfb0d95e7b24ad13416
ZaneWharton <zanewharton@gmail.com>
2025-06-29 19:00:17 -0500

    Style changes

--- frontend/src/components/TrackList.jsx
+++ frontend/src/components/TrackList.jsx
@@ -20,7 +20,7 @@ export default function TrackList({ tracks }) {
             </div>
 
             <div className="flex items-center justify-center bg-gray-800 bg-opacity-50 p-4 w-full sm:max-w-lg rounded-xl shadow-xl">
-                <iframe src={"https://open.spotify.com/embed/track/" + track.id} className="md:w-[425px] md:h-[365px] sm:h-[50px] sm:w-[75px] rounded-lg" allow="encrypted-media"></iframe>
+                <iframe src={"https://open.spotify.com/embed/track/" + track.id} className="w-full md:h-[365px] sm:h-[200px] rounded-lg" allow="encrypted-media"></iframe>
             </div>
             
             <div className="my-auto">
